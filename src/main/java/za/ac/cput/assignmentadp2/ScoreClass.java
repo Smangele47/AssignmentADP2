e3123f5d8659b37d1fa48e3d3b1e004e7c5f4658
Smangele47 <220047154@mycput.ac.za>
2021-04-01 18:06:24 +0200

    Commit by Smangele Mabaso

---
+++ src/main/java/za/ac/cput/assignmentadp2/ScoreClass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.assignmentadp2;
+
+/**
+ *
+ * @author SMANGELE MABASO 220047154
+ */
+public class ScoreClass {
+    
+    public int testEquality(int score1, int score2){
+        return score1 + score2;
+}
+   public int testIdentity(int score1, int score2){
+   return score1 = score2;
+   }
+   
+}
